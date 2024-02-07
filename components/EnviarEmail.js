8f41e27f7235a47bd36b6768f1ff9a2245c3e1b3
rodrigo7584 <rodrigo.gandhi@cojiba.com.br>
2024-02-07 17:51:17 -0300

    07/02/2024

--- components/EnviarEmail.js
+++ components/EnviarEmail.js
@@ -1,13 +1,12 @@
 'use client'
+// Importe o useState e useEffect se ainda não estiverem importados
 import { useState, useEffect } from 'react'
 
-interface EnviarEmailProps {}
-
-const EnviarEmail: React.FC<EnviarEmailProps> = () => {
+const EnviarEmail = () => {
   const [formData, setFormData] = useState({ name: '', number: '' })
-  const [message, setMessage] = useState('') 
+  const [message, setMessage] = useState('') // Estado para armazenar a mensagem
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async e => {
     e.preventDefault()
 
     try {
@@ -26,37 +25,28 @@ const EnviarEmail: React.FC<EnviarEmailProps> = () => {
       } else {
         setMessage(`Erro ao enviar mensagem: ${data.message}`)
       }
-    } catch (error:any) {
+    } catch (error) {
       setMessage('Erro na solicitação: ' + error.message)
     }
   }
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = e => {
     setFormData({ ...formData, [e.target.name]: e.target.value })
   }
 
   useEffect(() => {
+    // Limpa a mensagem após 3 segundos
     const timeoutId = setTimeout(() => {
       setMessage('')
     }, 3000)
 
-    return () => clearTimeout(timeoutId)
+    return () => clearTimeout(timeoutId) // Limpa o timeout ao desmontar o componente
   }, [message])
 
   return (
     <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="name"
-        placeholder="Informe seu nome"
-        onChange={handleChange}
-      />
-      <input
-        type="text"
-        name="number"
-        placeholder="Informe seu número"
-        onChange={handleChange}
-      />
+      <input type="text" name="name" placeholder="Informe seu nome" onChange={handleChange} />
+      <input type="text" name="number" placeholder="Informe seu número" onChange={handleChange} />
       <p>{message}</p> {/* Exibe a mensagem aqui */}
       <button type="submit" className="btn-green">
         Enviar
--- components/Header.js
+++ components/Header.js
@@ -156,3 +156,4 @@ const Header = () => {
     </header>
   )
 }
+export default Header
